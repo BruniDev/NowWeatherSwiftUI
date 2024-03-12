3cb8ae51843ab7b8e1348923b8a2a7c6476bfdca
BruniDev <hyeondxx@gmail.com>
2024-03-13 00:47:14 +0900

    feat: APIService 업로드

---
+++ WeatherApp/NetWork/NetworkService.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkService.swift
+//  WeatherApp
+//
+//  Created by 정현 on 3/13/24.
+//
+
+import SwiftUI
+
+struct NetworkService: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetworkService()
+}
---
+++ WeatherApp/NetWork/WebService.swift
@@ -0,0 +1,8 @@
+//
+//  WebService.swift
+//  WeatherApp
+//
+//  Created by 정현 on 3/13/24.
+//
+
+import Foundation
