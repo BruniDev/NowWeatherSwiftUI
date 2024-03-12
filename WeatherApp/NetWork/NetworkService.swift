f471ad6e3434424e7f435765fdb6a9cf52cd8f98
BruniDev <hyeondxx@gmail.com>
2024-03-13 00:48:13 +0900

    feat : Network 서비스 완료

--- WeatherApp/NetWork/NetworkService.swift
+++ WeatherApp/NetWork/NetworkService.swift
@@ -5,14 +5,39 @@
 //  Created by 정현 on 3/13/24.
 //
 
-import SwiftUI
+import Foundation
 
-struct NetworkService: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+typealias completion = (_ response: WeatherResponse?, _ error: Error?) -> Void
+
+protocol WeatherServiceProtocol {
+    func getWeather(_ latitude: String, _ longitude: String, completion: @escaping completion)
+    func getCityWeather(_ name: String, completion: @escaping completion)
 }
 
-#Preview {
-    NetworkService()
+struct NetworkService: WeatherServiceProtocol {
+    
+    func getWeather(_ latitude: String, _ longitude: String, completion: @escaping completion) {
+        WebService.request(Endpoint.weather(latitude: latitude, longitude: longitude), responseType: WeatherResponse.self) { result in
+            switch result {
+            case .success(let weahterResponse):
+                completion(weahterResponse, nil)
+            case .failure(let error):
+                // Handle the error
+                print("Error: \(error)")
+                completion(nil, nil)
+            }
+        }
+    }
+    
+    func getCityWeather(_ name: String, completion: @escaping completion) {
+        WebService.request(Endpoint.cityWeather(cityName: name), responseType: WeatherResponse.self) { result in
+            switch result {
+            case .success(let weahterResponse):
+                completion(weahterResponse, nil)
+            case .failure(let error):
+                print("Error: \(error)")
+                completion(nil, nil)
+            }
+        }
+    }
 }
