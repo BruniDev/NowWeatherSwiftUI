20f864d7b501903f103006a824d4fa390fe92aff
BruniDev <hyeondxx@gmail.com>
2024-03-22 00:45:32 +0900

    feat: 경도,위도에 따라 지리적 위치 가져오는것 완료

--- WeatherApp/View/WeatherView.swift
+++ WeatherApp/View/WeatherView.swift
@@ -7,13 +7,14 @@
 
 import SwiftUI
 import WeatherKit
-
+import CoreLocation
 
 struct WeatherView: View {
     
     @ObservedObject var weatherKitManager = WeatherKitManager()
     @State private var showingSheet = false
     @StateObject var locationManager = LocationManager()
+    @State var texting = ""
     var weatherUtils = WeatherUtils()
     
     var body: some View {
@@ -24,21 +25,24 @@ struct WeatherView: View {
                     .ignoresSafeArea()
                 if locationManager.authorizationStatus == .authorizedWhenInUse {
                     VStack(alignment: .leading){
-                        Text("서울특별시 광진구")
+                        Text(" ")
                             .task {
-                                await weatherKitManager.getWeather(latitude: locationManager.latitude, longtitude: locationManager.longtitude)
-                            }
-                            .position(x:geometryReader.size.width/3,y:geometryReader.size.height/10)
-                            .font(.system(size: 30,weight: .semibold))
-                        
-                        
-                        
+                                self.locationManager.reverseGeocoding(latitude: locationManager.latitude, longitude: locationManager.longtitude) {address in
+                                    print(address)
+                                    self.texting = address
+                                }
+                                    await weatherKitManager.getWeather(latitude: locationManager.latitude, longtitude: locationManager.longtitude)
+                                }
+                        Text(texting)
+                                .position(x:geometryReader.size.width/3,y:geometryReader.size.height/10)
+                                .font(.system(size: 30,weight: .semibold))
                         VStack{
                             Text("\(Int(weatherKitManager.temp))")
                                 .font(.system(size: 75,weight: .bold))
                             
                             Text("\(Image(systemName: "arrowtriangle.up.fill"))\(Int(weatherKitManager.highestTemp))  \(Int(weatherKitManager.lowestTemp))\(Image(systemName: "arrowtriangle.down.fill"))")
                                 .font(.system(size: 20,weight: .semibold))
+                            
                         }
                         .position(x:geometryReader.size.width/6 * 5,y: -geometryReader.size.height/8)
                         ScrollView(.horizontal) {
@@ -75,3 +79,26 @@ struct WeatherView: View {
 //#Preview {
 //    WeatherView(viewModel: WeatherInfoViewModel())
 //}
+
+extension WeatherView{
+    
+      func reverseGeocoding(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+              let geocoder = CLGeocoder()
+              let location = CLLocation(latitude: latitude, longitude: longitude)
+              geocoder.reverseGeocodeLocation(location) {(placemarks, error) in
+                  if error != nil {
+                      print("Failed to retrieve address")
+                      return
+                  }
+                  
+                  if let placemarks = placemarks, let placemark = placemarks.first {
+                      print(placemark.address!)
+                  }
+                  else
+                  {
+                      print("No Matching Address Found")
+                  }
+                  
+              }
+          }
+}
