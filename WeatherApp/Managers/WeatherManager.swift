f8e28013286e9e82786dcaacd2d9b007761a6ae4
BruniDev <hyeondxx@gmail.com>
2024-04-13 00:03:14 +0900

    feat: 기존 WeatherKitManager 에서 현위치에서 weather를 불러오는것만 적용

--- WeatherApp/Managers/WeatherManager.swift
+++ WeatherApp/Managers/WeatherManager.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import WeatherKit
+import CoreLocation
+import SwiftUI
+import Combine
+import WidgetKit
+ 
+public class WeatherManager : ObservableObject {
+    @Published var weather: Weather?
+    @Published var locationManager = LocationManager()
+    
+    private var anyCancellable : AnyCancellable? = nil
+    
+    init() {
+        anyCancellable = locationManager.objectWillChange.sink{[weak self] (_) in
+            self?.objectWillChange.send()
+            
+        }
+    }
+    var shortenedHourWeather: [HourWeather] {
+        if let weather {
+            return Array(weather.hourlyForecast.filter { hourlyWeather in
+                return hourlyWeather.date.timeIntervalSince(Date()) > 0
+            }.prefix(24))
+        } else {
+            return []
+        }
+    }
+    
+    
+    func requestWeatherForCurrentLocation() async {
+        guard let userLocation = locationManager.userLocation else { return }
+        do {
+            weather = try await WeatherService.shared.weather(for: userLocation)
+        } catch {
+            print("\(error.localizedDescription)")
+            weather = nil
+        }
+    }
+    
+    
+}
